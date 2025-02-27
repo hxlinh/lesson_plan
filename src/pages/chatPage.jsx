caca4624c0afeba169b6a20381fe800e19997b3d
Hoang Xuan Linh <xuanlinh.work@gmail.com>
2025-02-27 07:19:26 +0700

    update chat page

--- src/pages/chatPage.jsx
+++ src/pages/chatPage.jsx
@@ -4,29 +4,22 @@ import "../styles/chatPage.css"
 const ChatPage = () => {
   return (
     <div className="chat-page">
-      <div className="chat-container">
-        {/* Chat List Section */}
-        <div className="chat-list">
+      <div className="chat-container row">
+
+        <div className="chat-list col l-2">
           <div className="section-header">
             <h2>Đoạn Chat</h2>
           </div>
-          <div className="chat-users">
-            {[1, 2, 3].map((user) => (
+          <div className="chat-body">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((user) => (
               <div key={user} className="user-item">
-                <div className="user-avatar">
-                  <img src="/placeholder.svg" alt="avatar" />
-                </div>
-                <div className="user-info">
-                  <h3>Người dùng {user}</h3>
-                  <p className="last-message">Tin nhắn cuối...</p>
-                </div>
+                  <h3>Buổi {user}</h3>
               </div>
             ))}
           </div>
         </div>
 
-        {/* Chat Messages Section */}
-        <div className="chat-messages">
+        <div className="chat-messages col l-5">
           <div className="section-header">
             <h2>Cuộc trò chuyện</h2>
           </div>
@@ -37,72 +30,50 @@ const ChatPage = () => {
                 <img src="/placeholder.svg" alt="avatar" />
               </div>
               <div className="message-content">
-                <div className="message-bubble">
-                  <p className="message-sender">Đầu xanhh</p>
-                  <p className="message-text">Ghi lớn rồi =))</p>
-                  <span className="message-time">20:27 8 Tháng 2, 2025</span>
-                </div>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
+                <p className="message-text">Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))Ghi lớn rồi =))</p>
               </div>
             </div>
 
             <div className="message sent">
               <div className="message-content">
-                <div className="message-bubble">
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <p className="message-text">e oiw</p>
-                  <p className="message-text">cíu</p>
-                  <p className="message-text">ivy luege</p>
-                  <span className="message-time">23:29 8 Tháng 2, 2025</span>
-                </div>
+                  <p className="message-text">ivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luege</p>
+                  <p className="message-text">ivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luege</p>
+                  <p className="message-text">ivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luege</p>
+                  <p className="message-text">ivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luege</p>
+                  <p className="message-text">ivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luegeivy luege</p>
               </div>
             </div>
           </div>
 
-          <div className="message-input">
-            <input type="text" placeholder="Nhập tin nhắn..." />
-            <button className="send-button">
-              <span>Gửi</span>
-            </button>
-          </div>
+            <div className="message-input">
+              <input type="text" placeholder="Nhập tin nhắn..." />
+              <button className="send-button">
+                <span>Gửi</span>
+              </button>
+            </div>
         </div>
 
-        {/* Book Section */}
-        <div className="book-section">
+        <div className="book col l-5">
           <div className="section-header">
             <h2>Sách</h2>
           </div>
